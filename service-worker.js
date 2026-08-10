4a6b5f96d915eecff9971b9356de7dbb64622f8d
ASS-Ausgaben-a <hampistahl@gmail.com>
2026-08-10 14:55:57 +0200

    Assistenzausgaben A - PWA aktualisieren (service-worker.js)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/** Assistenzausgaben A - PWA Starter - erzeugt durch Modul 9 PWA v1.2 */
-const CACHE='assistenz-pwa-v1.2';
+/** Assistenzausgaben A - PWA Starter - erzeugt durch Modul 9 PWA v1.3 */
+const CACHE='assistenz-pwa-v1.3';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
